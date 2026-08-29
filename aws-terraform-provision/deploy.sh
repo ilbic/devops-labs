f6717d9d7227edc2dc28545f4bc5a78ce38fe3a5
judahhooper <judahhooper@icloud.com>
2026-08-29 10:32:49 +0100

    use heredoc instead of prinf

--- aws-terraform-provision/deploy.sh
+++ aws-terraform-provision/deploy.sh
@@ -4,8 +4,17 @@ set -euo pipefail
 
 terraform plan
 
-terraform apply -auto-approve
+terraform apply
 
 server_public_ip=$(terraform output -raw test_judah_sh_public_ip)
 
-echo "$server_public_ip"
+
+# tried printf here but this looks cleaner
+cat > inventory.yml <<EOF
+---
+all:
+  hosts:
+    test.judah.sh:
+      ansible_host: $server_public_ip
+EOF
+
